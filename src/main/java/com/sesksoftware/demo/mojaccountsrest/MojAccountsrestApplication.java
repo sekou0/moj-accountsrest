068aee6bdee41afea95b97bcb943870099716418
stanford <contact@sesksoftware.com>
2018-11-21 19:11:39 +0000

    Initial commit

---
+++ src/main/java/com/sesksoftware/demo/mojaccountsrest/MojAccountsrestApplication.java
@@ -0,0 +1,12 @@
+package com.sesksoftware.demo.mojaccountsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojAccountsrestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MojAccountsrestApplication.class, args);
+    }
+}
